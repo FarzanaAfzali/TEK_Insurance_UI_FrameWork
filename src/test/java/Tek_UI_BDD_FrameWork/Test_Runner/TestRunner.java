d77cc6938d9443975482c9a5546a6ad11f0b6cf0
Farzana Afzali <afzalifarzana297@gmail.com>
2023-11-01 20:59:11 -0400

    TEK Insurance App

--- src/test/java/Tek_UI_BDD_FrameWork/Test_Runner/TestRunner.java
+++ src/test/java/Tek_UI_BDD_FrameWork/Test_Runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
       features = "src/test/resources/Test_Features",
         glue = "",
-        tags = "",
+        tags = "@US_1",
         plugin = {
               "html:target/html_report/report.html",
                 "json:target/json_report/report.json"
